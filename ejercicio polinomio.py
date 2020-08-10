6b7c3628f69b87bc5cca003e65e0a3fe1c1a25a8
matarikii <47460104+matarikii@users.noreply.github.com>
2020-08-10 16:04:56 +0200

    Add files via upload

---
+++ ejercicio polinomio.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def polinomio (x):
+    resultado=x**4+x**3+(1/2*x**2)-x
+    return resultado
+
+
+print(polinomio(60))
